7c14c72d3149bdca416094c41bc16bc18b6d5a92
ananthvk <mailstoanantha@gmail.com>
2024-12-06 18:30:31 +0530

    Add another test

--- tests/cppstarter_test.cpp
+++ tests/cppstarter_test.cpp
@@ -11,6 +11,16 @@ TEST(TestCppstarter, Addition)
     ASSERT_EQ(result, 10);
 }
 
+TEST(TestCppstarter, Addition2)
+{
+    auto result = cppstarter::add(3, 5);
+    ASSERT_EQ(result, 8);
+    result = cppstarter::add(4, 5);
+    ASSERT_EQ(result, 9);
+    result = cppstarter::add(5, 5);
+    ASSERT_EQ(result, 10);
+}
+
 int main(int argc, char *argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
